1257e51631a48ff76780b8c72c182576400a89f0
unknown <hcmiki358@gmail.com>
2023-07-06 16:53:59 +0200

    removed classes and changed year

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -6,24 +6,25 @@ import { IoIosArrowUp } from "react-icons/io";
 
 const Footer = () => {
   const { lang } = useContext(LangContext);
+  const year = new Date().getFullYear();
+
   return (
     <footer className="bg-zinc-900 w-full p-12">
       <div className="text-center text-white flex flex-col gap-2 sm:flex-row items-center justify-between">
         <p className="md:text-lg">
           {lang === "pl"
-            ? "Wszelkie prawa zastrzeżone © 2023"
-            : "All rights reserved © 2023"}
+            ? `Wszelkie prawa zastrzeżone © ${year}`
+            : `All rights reserved © ${year}`}
         </p>
 
         <div className="flex gap-2">
           <a
             href="https://www.linkedin.com/in/mikolaj-malarczyk/"
             target="_blank"
-            className=""
           >
             <SiLinkedin size={30} />
           </a>
-          <a href="https://github.com/malalalash" target="_blank" className="">
+          <a href="https://github.com/malalalash" target="_blank">
             <SiGithub size={30} />
           </a>
           <Link
